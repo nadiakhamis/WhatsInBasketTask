94646eb71dd76c3eb6c57711cd8a7758f8849949
nadiakhamis <129910536+nadiakhamis@users.noreply.github.com>
2023-04-15 03:34:09 +0200

    remove jQ

--- js/main.js
+++ js/main.js
@@ -6,83 +6,55 @@ const reloadBtn = document.querySelector(".reloadBtn");
 const closeBtn = document.querySelectorAll(".closeBtn");
 const questions = document.querySelectorAll(".question");
 const options = document.querySelectorAll(".option");
-const resetAllBtn = $("#resetAll").first();
-const viewAllBtn = $("#viewAll").first();
-viewAllBtn.on("click", viewAll);
-resetAllBtn.on("click", resetAll);
 let currAns = null;
 let isCorrect = false;
+let timeoutId;
 var pageWidth, pageHeight;
-const correctAudio = $("<audio id=`correctAudio`>").attr("src", "./assets/audio/correct.mp3").get(0);
-const inCorrectAudio = $("<audio>").attr("src", "./assets/audio/incorrect.mp3").get(0);
-
-$(".myContent, .resourseContainer, .helpContainer").css("height",(960 * 74) / 100 + 15 + "px");
-$(".backGround").css("height", (960 * 74) / 100 + "px");
-$(".header, .footer").css("height", (960 * 13) / 100 + "px");
-
+const correctAudio = document.querySelector("#correctAudio");
+const inCorrectAudio = document.querySelector("#InCorrectAudio");
+var pageElement = document.querySelector(".myPage");
 
 var basePage = {
   width: 1280,
   height: 960,
-  scale: 1,
-  scaleX: 1,
-  scaleY: 1,
+  scale: 0,
+  scaleX: 0,
+  scaleY: 0,
 };
-
-const choices = [
-  { title: "scissors", isCorrect: false, selected: false },
-  { title: "eraser", isCorrect: true, selected: false },
-  { title: "ruler", isCorrect: true, selected: true },
-  { title: "bag", isCorrect: false, selected: false },
-  { title: "pencil pen", isCorrect: false, selected: true },
-  { title: "pencil", isCorrect: true, selected: false },
-  { title: "book", isCorrect: true, selected: false },
-  { title: "pen", isCorrect: true, selected: false },
-];
-
-
-$(function () {
-  var $page = $(".myPage");
-
+window.addEventListener("load", function () {
   getPageSize();
-  scalePages($page, pageWidth, pageHeight);
-  
-  $(window).resize(
-    _.debounce(function () {
-      getPageSize();
-      scalePages($page, pageWidth, pageHeight);
-    }, 150)
-  );
-
-  function getPageSize() {
-    pageHeight = $("#container").height();
-    pageWidth = $("#container").width();
-  }
-
-  function scalePages(page, maxWidth, maxHeight) {
-    var scaleX = 1,
-      scaleY = 1;
-    scaleX = maxWidth / basePage.width;
-    scaleY = maxHeight / basePage.height;
-    basePage.scaleX = scaleX;
-    basePage.scaleY = scaleY;
-    basePage.scale = scaleX > scaleY ? scaleY : scaleX;
-    var newLeftPos = Math.abs(
-      Math.floor((basePage.width * basePage.scale - maxWidth) / 2)
-    );
-    var newTopPos = 0;
-    page.attr(
-      "style",
-      "-webkit-transform:scale(" +
-        basePage.scale +
-        ");left:" +
-        newLeftPos +
-        "px;top:" +
-        newTopPos +
-        "px;"
-    );
-  }
+  scalePages(pageElement, pageWidth, pageHeight);
 });
+
+window.addEventListener("resize", function () {
+  let timeoutId;
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(function () {
+    getPageSize();
+    scalePages(pageElement, pageWidth, pageHeight);
+  }, 150);
+});
+
+function getPageSize() {
+  pageHeight = document.querySelector("#container").clientHeight;
+  pageWidth = document.querySelector("#container").clientWidth;
+}
+
+function scalePages(pageElement, maxWidth, maxHeight) {
+  var scaleX = 1,
+    scaleY = 1;
+  scaleX = maxWidth / basePage.width;
+  scaleY = maxHeight / basePage.height;
+  basePage.scaleX = scaleX;
+  basePage.scaleY = scaleY;
+  basePage.scale = scaleX > scaleY ? scaleY : scaleX;
+  var newLeftPos = Math.abs(
+    Math.floor((basePage.width * basePage.scale - maxWidth) / 2)
+  );
+  var newTopPos = 0;
+  let transform = "-webkit-transform:scale(" + basePage.scale +");left:" + newLeftPos +"px;top:" + newTopPos + "px;";
+  pageElement.setAttribute("style",transform);
+}
 
 resourseBtn.addEventListener("click", function () {
   resoursePopup.classList.add("show");
@@ -122,21 +94,16 @@ options.forEach(function (currentOption) {
     }
     if (isCorrect) {
       let currSelectedQues = document.querySelector(".question.selected");
-      currentOption.innerHTML =
-        currAns + "<img src='assets/images/tikMark-small.png'/>";
+      currentOption.innerHTML = currAns + "&nbsp;" + "<img src='assets/images/tikMark-small.png'/>";
       correctAudio.play();
 
       currentOption.classList.add("done");
-      currentOption.classList.add("correctAnsAudio");
-
       currSelectedQues.classList.add("hidden");
       currSelectedQues.classList.remove("selected");
       currAns = null;
       isCorrect = false;
     } else {
-      currentOption.innerHTML =
-        currAns +
-        "<img class='animate' src='assets/images/crossMark-small.png'/>";
+      currentOption.innerHTML = currAns + "&nbsp;" + "<img class='animate' src='assets/images/crossMark-small.png'/>";
       inCorrectAudio.play();
 
       setTimeout(function () {
@@ -158,32 +125,31 @@ reloadBtn.addEventListener("click", function () {
 
 function viewAll() {
   let count = 0;
-  const lineItem = document.querySelectorAll(".optionContainer .option");
-  choices.forEach((item, index, array) => {
-    if (item.isCorrect) {
+  questions.forEach((item, index, array) => {
+    let dataAns = item.getAttribute("data-answer");
+    if (dataAns == "correct") {
       const markImg = document.createElement("img");
       markImg.id = index;
       markImg.src = "./assets/images/tikMark-small.png";
 
       let isSelectedOption = false;
-      lineItem.forEach(function (currentOptions) {
-        if (item.title == currentOptions.textContent) {
+      options.forEach(function (currentOptions) {
+        if (item.innerHTML == currentOptions.textContent) {
           isSelectedOption = true;
           return;
         }
       });
-      if(!isSelectedOption){
-        if(isEmptyOrSpaces(lineItem[count].textContent)){
-          lineItem[count].classList.add("done");
-          lineItem[count].textContent = item.title;
-          lineItem[count].append(markImg);
+      if (!isSelectedOption) {
+        if (isEmptyOrSpaces(options[count].textContent)) {
+          options[count].classList.add("done");
+          options[count].textContent = item.innerHTML;
+          options[count].append(markImg);
           count++;
-  
-        }else{
-          count++;  
-          lineItem[count].classList.add("done");
-          lineItem[count].textContent = item.title;
-          lineItem[count].append(markImg);
+        } else {
+          count++;
+          options[count].classList.add("done");
+          options[count].textContent = item.innerHTML;
+          options[count].append(markImg);
         }
       }
     }
@@ -196,19 +162,12 @@ function viewAll() {
   });
 
   correctAudio.play();
-  $(".optionContainer").off("click");
-}
-
-function resetAll() {
-  const lineItem = $(".optionContainer");
-  lineItem.each(function () {
-    $(this).removeClass("selected");
-    $(this).text("");
+  const optionContainers = document.querySelectorAll(".optionContainer");
+  optionContainers.forEach((optionContainer) => {
+    optionContainer.removeEventListener("click", function () {});
   });
-  $("#components__container li").removeClass("active");
-  renderPage(true);
 }
 
-function isEmptyOrSpaces(str){
-  return str === null || str.trim() === '';
+function isEmptyOrSpaces(str) {
+  return str === null || str.trim() === "";
 }
